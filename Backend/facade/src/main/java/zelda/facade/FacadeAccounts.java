140e0270b15e540baef6fc06ba890621bee66dad
Verigyk <lygregory@hotmail.fr>
2026-03-30 12:50:27 +0200

    Système de compte fonctionnel

--- Backend/facade/src/main/java/zelda/facade/FacadeAccounts.java
+++ Backend/facade/src/main/java/zelda/facade/FacadeAccounts.java
@@ -9,23 +9,22 @@ import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-
 
 @RestController
+@RequestMapping("/accounts")
 public class FacadeAccounts {
     @Autowired
     AccountRepository ar;
 
-    @PostMapping("addAccount")
+    @PostMapping("/addAccount")
     public void addAccount(Account a) {
         ar.save(a);
     }
 
-    @GetMapping("listAccounts")
-    public Collection<Account> listAccounts(@RequestParam String param) {
+    @GetMapping("/listAccounts")
+    public Collection<Account> listAccounts() {
         return ar.findAll();
     }
     
